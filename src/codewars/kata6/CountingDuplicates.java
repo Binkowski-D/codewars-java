1bb9f710530609c828a8eab0c7333c42af5deeae
Binkowski-D <binkowski.dam@gmail.com>
2025-01-23 16:57:03 +0100

    Implement duplicate character counting with two approaches

---
+++ src/codewars/kata6/CountingDuplicates.java
@@ -0,0 +1,44 @@
+package codewars.kata6;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountingDuplicates {
+    public static void main(String[] args) {
+
+        System.out.println(duplicateCountSecondOption("aA11"));
+    }
+
+    public static int duplicateCount(String text) {
+        String lower = text.toLowerCase();
+        char[] chars = lower.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for(char tmp : chars){
+            map.put(tmp, map.getOrDefault(tmp, 0) + 1);
+        }
+
+        int result = 0;
+
+       for(Map.Entry<Character, Integer> tmp : map.entrySet()){
+           if(tmp.getValue() > 1){
+               result ++;
+           }
+       }
+        return result;
+    }
+
+    public static int duplicateCountSecondOption(String text){
+
+        return (int) text.toLowerCase()
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .values()
+                .stream().filter(count -> count > 1)
+                .count();
+
+
+    }
+}
